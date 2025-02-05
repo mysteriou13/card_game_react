b5c4b4ae7cd938b8bfa59f569855dad4810f3721
Massa anthony <elio007@hotmail.fr>
2025-02-05 16:07:37 +0100

    ajout css dynamiqie pour les carte

--- src/app/components/Carte/index.tsx
+++ src/app/components/Carte/index.tsx
@@ -4,6 +4,7 @@ interface CarteProps {
   data: {
     famille_aleatoire: string;
     valeur_aleatoire: string;
+    icone_carte:string;
     img: string;
   };
 }
@@ -14,16 +15,24 @@ composant carte
 export default function Carte({ data }: CarteProps) {
   // Conversion de la valeur en nombre pour déterminer le nombre d'images
   let nombreImages = parseInt(data.valeur_aleatoire); // Convertir en nombre
-  
   // Créer un tableau avec le nombre d'images
   let imagesArray = Array.from({ length: nombreImages }, () => data.img);
-  
+
   // Vérifier si la valeur est une lettre (A-Z)
   let verif_type_carte = /^[a-zA-Z]$/.test(data.valeur_aleatoire);
+
+  let img_card:string;
+
+   let box_card:string
 
   if (verif_type_carte) {
     // Si la valeur est une lettre, ajouter l'image une fois
     imagesArray = [data.img];
+    img_card = "One_image_card"
+    box_card = null;
+  }else{
+    box_card = "box_card_img";
+    img_card = "div_img_card";
   }
 
   return (
@@ -32,34 +41,32 @@ export default function Carte({ data }: CarteProps) {
       {/* Affichage de la valeur en haut à gauche */}
       <div className="top-left">
          <div>
-         {data.valeur_aleatoire}
+        
+         <div>
+          
+          {data.valeur_aleatoire} <img src = {data.icone_carte} className="img_icone_top"/>
+          
+          </div>
+        
         </div>
+        
       </div>
 
       {/* Génération dynamique des images avec grid */}
       
 
       {/* afficher des nombre*/}
-      {imagesArray.length >=2 &&  
-       <div className="box_card_img">
+  
+       <div className={box_card}>
        {imagesArray.map((imgSrc, index) => (
-         <div key={index} className="div_img_card">
+         <div key={index} className={img_card}>
            <img className="img_card" src={imgSrc} alt={`Carte ${index + 1}`} />
          </div>
        ))}
      </div>
-      }
+      
 
-   {/* afficher des figure */}
-     {imagesArray.length == 1 &&  
-       <div>
-       {imagesArray.map((imgSrc, index) => (
-         <div key={index} className="One_image_card">
-           <img className="img_card" src={imgSrc} alt={`Carte ${index + 1}`} />
-         </div>
-       ))}
-     </div>
-      }
+
 
 
       {/* Affichage de la valeur en bas à droite */}
